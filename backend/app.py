55b443a4d609aa024d533e2d7b367e874124d3b5
Donbasilpeter <donbasilpeter@gmail.com>
2021-11-06 14:19:59 +0530

    get price of stock by id and date range

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,48 @@
-from flask import Flask
+from flask import Flask,request
 import requests
+import json
+import datetime
 
 
-scrip_code = 500110
-from_date = 20120301
-to_date =  20130530
-api_url = 'https://api.bseindia.com/BseIndiaAPI/api/StockReachGraph/w?scripcode=' + str(scrip_code) + '&flag=1&fromdate=' +str(from_date) + '&todate=' +str(to_date) +  '&seriesid='
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+
+#get bse daily data using bse stock code and dates input formaat {  "script_code": "500222",  "from_date": "20130301",  "to_date": "20130530"}
+
+@app.route('/data', methods = ['POST'])
 def getdata():
+    data = request.json
+    script_code = data.get('script_code')
+    from_date = data.get('from_date')
+    to_date = data.get('to_date')
+    api_url = 'https://api.bseindia.com/BseIndiaAPI/api/StockReachGraph/w?scripcode=' + str(script_code) + '&flag=1&fromdate=' +str(from_date) + '&todate=' +str(to_date) +  '&seriesid='
     response = requests.get(api_url, headers={'User-Agent' : ''})
-    return response.content
+    responsedata = json.loads(response.content)
+    responsedata = responsedata["Data"]
+    responsedata =json.loads(responsedata)
+    start_date = datetime.date(int(from_date[0:4]), int(from_date[4:6]),int(from_date[6:8]))
+    end_date = datetime.date(int(to_date[0:4]), int(to_date[4:6]),int(to_date[6:8]))
+    delta = datetime.timedelta(days=1)
+    i= 0
+    length = len(responsedata)
+    pricedate=[]
+    while start_date <= end_date:
+        
+        datadate =datetime.datetime.strptime(responsedata[i]['dttm'][0:15], '%a %b %d %Y').strftime('%Y-%m-%d')
+        if datadate == str(start_date):
+            pricedate.append({"date": start_date, "price" : responsedata[i]['vale1'],"type" : True})   #use str(start_date) or startdate to chance format
+            if i < length-1:
+                i=i+1
+        else:
+            pricedate.append({"date": start_date, "price" : "" ,"type":False})
+        start_date += delta
+    pricedate = {"pricedata" :pricedate}
+   
+    return pricedate
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
