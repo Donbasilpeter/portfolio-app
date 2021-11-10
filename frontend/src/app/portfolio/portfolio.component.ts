4f7263c26d87ead57e55b1559f1405b5fd778e82
Donbasilpeter <donbasilpeter@gmail.com>
2021-11-10 20:40:15 +0530

    changed into index values

--- frontend/src/app/portfolio/portfolio.component.ts
+++ frontend/src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,3 @@
-import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit,Input,Output,EventEmitter  } from '@angular/core';
 
 @Component({
@@ -14,6 +13,8 @@ export class PortfolioComponent implements OnInit {
    temp:any = 0;
   @Input() currentstocks:any;
   @Output() currentstocksChange = new EventEmitter<any>();
+  displayall:boolean =false;
+
 
   constructor() { }
 
@@ -21,32 +22,82 @@ export class PortfolioComponent implements OnInit {
 
   }
   quantityload(){
+    let execute = true;
     this.result = []
+    let arraysum:number = 0;
+    for(let i = 0; i<this.quantity.length;i++){
+      arraysum = Number(arraysum) + Number(this.quantity[i]);
+  
+    
+
+    }
+    this.quantity.forEach((element:any) => {
+      if(element<0){
+        execute =false;
+      }
+    });
+    if(execute){
+      
+    if(!(arraysum-100 >0.5 || arraysum-100 <0.5) ){
+      alert("Sum of quantity of all stocks must be 100. You have a difference of " + (arraysum -100) + "%");
+    }
+    else{
+      
     for(let eachprice = 0; eachprice < (this.currentstocks[0]).pricedata.length; eachprice++ ){
       this.currentstocks.forEach((element:any = 0, index:any =0) => {
-        this.temp = this.temp + (element.pricedata[eachprice]).price * this.quantity[index];
+        this.temp = this.temp + element.pricedata[eachprice].price * this.quantity[index]/100;
       });
-      this.result.push({ month:this.currentstocks[0].pricedata[eachprice].date.slice(5, 16) , price: this.temp })
+      this.result.push({ date:this.currentstocks[0].pricedata[eachprice].date.slice(5, 16) , portfolio: this.temp })
       this.temp = 0;
       
     }
-    console.log(this.currentstocks,this.result);
-  }
-  
-  onChange(){
-    this.totalamount = 0;
-    this.currentstocks.forEach((element:any, index:any) => {
-      this.totalamount = this.totalamount + (element.pricedata[0]).price * this.quantity[index];
-     
-    });
- 
+    if(this.displayall){
+
+
+      this.displayallstocks();
+    }
+    }
+    }
+    else{
+      alert("no negative values");
+    }
     
   }
+
+  onchange(i:number){
+    let sum = 0
+    for(let start=0;start <=i; start++ ){
+      sum = sum + this.quantity[start]
+    }
+    for(let start = i+1;start<this.quantity.length;start++){
+      this.quantity[start] = (100 -sum)/(this.quantity.length -i-1); 
+    }
+  }
   remove(i:number){
+    let val = this.quantity[i];
+    for(let itrate = 0; itrate<this.quantity.length;itrate++){
+      this.quantity[itrate] = this.quantity[itrate] + val/(this.quantity.length - 1) 
+    
+    }
     this.currentstocks.splice(i, 1);
     this.currentstocksChange.emit(this.currentstocks)
     this.quantity.splice(i, 1);
     this.quantityChange.emit(this.quantity)
+    
   }
 
+
+  displayallstocks(){
+    let resultcopy:any = [];
+   let eachdata:any ;
+    this.result.forEach((element:any,index:any) => {
+      this.currentstocks.forEach((stock:any) => {
+        eachdata = element;
+        eachdata[stock.name] =stock.pricedata[index].price
+        resultcopy.push(eachdata) 
+      });
+    });
+    this.result = resultcopy
+    console.log(this.result)
+  }
 }
--- frontend/src/app/search-stocks/search-stocks.component.ts
+++ frontend/src/app/search-stocks/search-stocks.component.ts
@@ -14,13 +14,14 @@ export class SearchStocksComponent implements OnInit {
   quantity:any = []
   do:boolean = true;
   bsecode:any = ""
-
+ 
   
 
   constructor(private request: RequestService) { }
   
   ngOnInit(): void {
     this.getbsecode();
+    
   }
 
   getbsecode(){
@@ -47,20 +48,28 @@ getdata(input:string,fromdate:string,todate:string){
   todate = todate.slice(0, 4) + todate.slice(5, 7) + todate.slice(8, 10);
   fromdate = fromdate.slice(0, 4) + fromdate.slice(5, 7) + fromdate.slice(8, 10);
   this.request.getdata(input,fromdate,todate).subscribe(data =>{
+
+    let startprice = data.pricedata[0].price;
     this.currentstocks.forEach( (element:any) => {
       if(data.script_code == element.script_code){
         this.do = false;
       }
   });
   if(this.do){
-    this.currentstocks.push(data);
+    let pricedata:any = []
+    data.pricedata.forEach((element:any,index:any) => {
+      pricedata.push({date:element.date,price:element.price * 100/startprice,type:element.type})
+    });
+    this.currentstocks.push({name:data.name, pricedata: pricedata,script_code:data.script_code,startprice:startprice })
+  
+  
     this.quantity.push(0);
     
   }
   this.do = true;   
 
   })
-  
+
 
 }
     
@@ -72,11 +81,17 @@ getdata(input:string,fromdate:string,todate:string){
     fromdate = fromdate.slice(0, 4) + fromdate.slice(5, 7) + fromdate.slice(8, 10);
     this.currentstocks.forEach((element:any) => {
       this.request.getdata(element.script_code,fromdate,todate).subscribe(data =>{
-        this.temparray.push(data);
-  
+      let startprice = data.pricedata[0].price;
+      let pricedata:any = [];
+      data.pricedata.forEach((each:any,index:any) => {
+        pricedata.push({date:each.date,price:each.price * 100/startprice,type:each.type})
+      });
+      this.temparray.push({name:data.name, pricedata: pricedata,script_code:data.script_code,startprice:startprice })
+      
         });
     });
     this.currentstocks = this.temparray;
+
   }
 
 
